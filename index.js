da841345fb3c6c72a0d67223646ca18e67f80043
Japsimran Singh <japsimrans13@gmail.com>
2022-01-29 14:46:08 +0530

    cors added

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const axios = require('axios');
 const PORT = process.env.PORT ||5000;
 const app = express();
+var cors = require('cors');
 PUBLIC_API_KEY = '1e1e4f04330a494b8a8da5572434153b'
 
-
+app.use(cors());
 app.get('/news', async (req, res)=> {
     res.setHeader('Access-Control-Allow-Origin', 'https://all-in-one-news.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET');
